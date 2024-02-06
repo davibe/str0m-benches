089784bab8640f9e3e17b54c62444fd39b1028fd
Davide Bertola <dade@dadeb.it>
2024-02-06 16:22:24 +0100

    ..

--- benches/bench0.rs
+++ benches/bench0.rs
@@ -5,8 +5,10 @@ use common::pair::LtoR;
 
 use divan::{black_box, Bencher};
 
+#[cfg(feature = "allocations")]
 use divan::AllocProfiler;
 
+#[cfg(feature = "allocations")]
 #[global_allocator]
 static ALLOC: AllocProfiler = AllocProfiler::system();
 
@@ -15,7 +17,7 @@ pub fn vp8_unidir(bencher: Bencher) {
     let server = LtoR::with_vp8_input();
     let server_ref = &server;
 
-    bencher.bench(|| {
+    bencher.bench_local(|| {
        let _ = black_box(black_box(server_ref).run().expect("error")); 
     });
 }
@@ -25,7 +27,7 @@ pub fn vp9_unidir(bencher: Bencher) {
     let server = LtoR::with_vp9_input();
     let server_ref = &server;
 
-    bencher.bench(|| {
+    bencher.bench_local(|| {
        let _ = black_box(black_box(server_ref).run().expect("error")); 
     });
 }
--- benches/scripts/run.sh
+++
@@ -1,2 +0,0 @@
-cargo +nightly add --package client str0m --git https://github.com/algesten/str0m --rev $TARGET --ignore-rust-version -Zmsrv-policy
-cargo bench
